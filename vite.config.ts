c5b9d0bb11578bd2c30b370f1aadba4471e1f4e0
smileleft <spyt77@gmail.com>
2025-11-19 14:08:48 +0900

    feature: 1st commit for cesiumjs with react

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import cesium from "vite-plugin-cesium"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }), cesium()
+  ],
+})
